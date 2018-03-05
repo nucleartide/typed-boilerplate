503826bbf4b7b4e8ea7a7258cfa8960d2194b090
Jason Tu <jasontu4@gmail.com>
2018-03-04 23:09:07 -0500

    Add component example

---
+++ src/components/example.js
@@ -0,0 +1,13 @@
+/*
+import type { Todo as Model } from '../types/todo';
+
+type Props = Todo & {
+};
+
+// <YourComponent> does something.
+const YourComponent = (p: Props) => (
+  <div>hello world</div>
+);
+
+export default YourComponent;
+*/
